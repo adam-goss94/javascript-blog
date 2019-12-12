a21db2f91a12ee6ed610cacc0db88475ba5d293f
Adam Goss <adam.goss94@gmail.com>
2019-12-12 17:48:22 +0100

    Add generateTitleLinks function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
 'use strict';
 {
+  const optArticleSelector = '.post',
+        optTitleSelector = '.post-title',
+        optTitleListSelector = '.titles';
+
+  const generateTitleLinks = () => {
+    /* remove contents of titleList */
+    document.querySelector(optTitleListSelector).innerHTML = '';
+
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
+
+    for(let article of articles){
+        /* get the article id */
+        const articleId = article.getAttribute('id');
+        
+        /* find the title element and get the title from the title element */
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+        
+        /* create HTML of the link */
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+        /* insert link into variable */
+        html = html + linkHTML;
+    }
+    document.querySelector(optTitleListSelector).innerHTML = html;
+  }
+  generateTitleLinks();
+
+
   const titleClickHandler = function(event){
     event.preventDefault();
     
@@ -25,5 +55,4 @@
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
-  
 }
