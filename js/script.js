d0a791c54c55db732d3f82961ba2bdba0536c0f2
Adam Goss <adam.goss94@gmail.com>
2019-12-12 16:59:08 +0100

    Add titleClickHandler function to script.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,23 @@
 'use strict';
-
-
-/* document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-  }); */
-
+{
   const titleClickHandler = function(event){
-    console.log('Link was clicked!');
-  
-    /* remove class 'active' from all article links  */
+    event.preventDefault();
+    
     const activeLinks = document.querySelectorAll('.titles a.active');
-
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    this.classList.add('active');
+   
     const activeArticles = document.querySelectorAll('.posts article.active');
-
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
-    }
-    /* remove class 'active' from all articles */
-  
-    /* get 'href' attribute from the clicked link */
-  
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
+    } 
+    
+    const articleSelector = this.getAttribute('href');
+    const targetArticle = document.querySelector(articleSelector);
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
@@ -36,3 +25,5 @@
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+  
+}
