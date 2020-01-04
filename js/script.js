6401d9e089115332f93a207a847036b9ff75f783
Adam Goss <adam.goss94@gmail.com>
2020-01-04 12:24:45 +0100

    Add template for generateTags function

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@
 {
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
-    optTitleListSelector = '.titles';
+    optTitleListSelector = '.titles',
+    optArticleTagsSelector = '.post-tags .list';
+
+
   const generateTitleLinks = () => {
     /* remove contents of titleList */
     document.querySelector(optTitleListSelector).innerHTML = '';
@@ -50,4 +53,32 @@
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+  function generateTags(){
+    /* find all articles */
+
+    /* START LOOP: for every article: */
+
+      /* find tags wrapper */
+
+      /* make html variable with empty string */
+
+      /* get tags from data-tags attribute */
+
+      /* split tags into array */
+
+      /* START LOOP: for each tag */
+
+        /* generate HTML of the link */
+
+        /* add generated code to html variable */
+
+      /* END LOOP: for each tag */
+
+      /* insert HTML of all the links into the tags wrapper */
+
+    /* END LOOP: for every article: */
+  }
+
+  generateTags();
 }
