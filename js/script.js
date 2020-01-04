97297de907a0ce08fe972d1cad67d9c9956519f0
Adam Goss <adam.goss94@gmail.com>
2020-01-04 12:04:53 +0100

    Install EditorConfig

--- js/script.js
+++ js/script.js
@@ -12,9 +12,9 @@
     for(let article of articles){
       /* get the article id */
       const articleId = article.getAttribute('id');
-        
+
       /* find the title element and get the title from the title element */
-      const articleTitle = article.querySelector(optTitleSelector).innerHTML;        
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
       /* create HTML of the link */
       const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
       /* insert link into variable */
@@ -27,26 +27,26 @@
 
   const titleClickHandler = function(event){
     event.preventDefault();
-    
+
     const activeLinks = document.querySelectorAll('.titles a.active');
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
 
     this.classList.add('active');
-   
+
     const activeArticles = document.querySelectorAll('.posts article.active');
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
-    } 
-    
+    }
+
     const articleSelector = this.getAttribute('href');
     const targetArticle = document.querySelector(articleSelector);
     targetArticle.classList.add('active');
   };
-  
+
   const links = document.querySelectorAll('.titles a');
-  
+
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
