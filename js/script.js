54bdf81f749fee997c02820f51b39b8e1f4b7199
Adam Goss <adam.goss94@gmail.com>
2020-01-04 11:58:08 +0100

    Fix errors generated by eslint in js files

--- js/script.js
+++ js/script.js
@@ -1,32 +1,27 @@
 'use strict';
 {
   const optArticleSelector = '.post',
-        optTitleSelector = '.post-title',
-        optTitleListSelector = '.titles';
-
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
   const generateTitleLinks = () => {
     /* remove contents of titleList */
     document.querySelector(optTitleListSelector).innerHTML = '';
-
     /* for each article */
     const articles = document.querySelectorAll(optArticleSelector);
     let html = '';
-
     for(let article of articles){
-        /* get the article id */
-        const articleId = article.getAttribute('id');
+      /* get the article id */
+      const articleId = article.getAttribute('id');
         
-        /* find the title element and get the title from the title element */
-        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-        
-        /* create HTML of the link */
-        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-
-        /* insert link into variable */
-        html = html + linkHTML;
+      /* find the title element and get the title from the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;        
+      /* create HTML of the link */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      /* insert link into variable */
+      html = html + linkHTML;
     }
     document.querySelector(optTitleListSelector).innerHTML = html;
-  }
+  };
   generateTitleLinks();
 
 
@@ -35,20 +30,20 @@
     
     const activeLinks = document.querySelectorAll('.titles a.active');
     for(let activeLink of activeLinks){
-        activeLink.classList.remove('active');
+      activeLink.classList.remove('active');
     }
 
     this.classList.add('active');
    
     const activeArticles = document.querySelectorAll('.posts article.active');
     for(let activeArticle of activeArticles){
-        activeArticle.classList.remove('active');
+      activeArticle.classList.remove('active');
     } 
     
     const articleSelector = this.getAttribute('href');
     const targetArticle = document.querySelector(articleSelector);
     targetArticle.classList.add('active');
-  }
+  };
   
   const links = document.querySelectorAll('.titles a');
   
