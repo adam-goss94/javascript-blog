8c06aab324f63b42a9a5d1a117f7fb83ab78ed77
Adam Goss <adam.goss94@gmail.com>
2020-01-04 13:09:42 +0100

    Add content to generateTags function

--- js/script.js
+++ js/script.js
@@ -55,30 +55,22 @@
   }
 
   function generateTags(){
-    /* find all articles */
+    const articles = document.querySelectorAll(optArticleSelector);
 
-    /* START LOOP: for every article: */
+    for(let article of articles){
+      const articleTitle = article.querySelector(optArticleTagsSelector);
+      let html = '';
+      const articleTags = article.getAttribute('data-tags');
+      const articleTagsArray = articleTags.split(' ');
 
-      /* find tags wrapper */
+      for(let tag of articleTagsArray){
+        const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+        html = html + linkHTML;
+      }
 
-      /* make html variable with empty string */
-
-      /* get tags from data-tags attribute */
-
-      /* split tags into array */
-
-      /* START LOOP: for each tag */
-
-        /* generate HTML of the link */
-
-        /* add generated code to html variable */
-
-      /* END LOOP: for each tag */
-
-      /* insert HTML of all the links into the tags wrapper */
-
-    /* END LOOP: for every article: */
+      articleTitle.innerHTML = html;
+    }
   }
-
   generateTags();
+
 }
